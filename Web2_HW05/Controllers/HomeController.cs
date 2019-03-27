5520462b3ebc5afb13bc0a56b8fc8d008b9a06b5
Jonathan Allen <jallen@snow.edu>
2019-03-27 13:16:43 -0600

    Samples for use in class

--- Web2_HW05/Controllers/HomeController.cs
+++ Web2_HW05/Controllers/HomeController.cs
@@ -2,16 +2,59 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using Refit;
+using ServiceReference;
 using Web2_HW05.Models;
 
 namespace Web2_HW05.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly IConfiguration configuration;
+
+        public HomeController(IConfiguration configuration)
         {
+            this.configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = new NumberConversionSoapTypeClient(NumberConversionSoapTypeClient.EndpointConfiguration.NumberConversionSoap);
+            var dollars = await client.NumberToDollarsAsync(12345.67M);
+            ViewData["dollars"] = dollars.Body.NumberToDollarsResult;
+
+
+            var httpClient = new HttpClient();
+            var json = await httpClient.GetStringAsync("http://api.walmartlabs.com/v1/search?query=ipod&format=json&apiKey=mb94unfjgxety22grbux6zb4");
+            var walmartSearchResult = new
+            {
+                Query = "",
+                TotalResults = 1,
+                Start=1,
+                NumItems=1,
+                Items=new[]{
+                    new {
+                        ItemId=1,
+                        Name="",
+                        Msrp=1M,
+                        SalePrice=1M,
+                        Upc="",
+                        CategoryPath="",
+                        ShortDescription="",
+                        ThumbnailImage=""
+                    }
+                }
+            };
+            var searchResults = JsonConvert.DeserializeAnonymousType(json, walmartSearchResult);
+
+            var walmartApi = RestService.For<IWalmartSearchApi>("http://api.walmartlabs.com");
+            var walmartResults = await walmartApi.SearchAsync("maple syrup", configuration["keys:walmart"]);
+            ViewData["walmartResults"] = walmartResults;
             return View();
         }
 
---
+++ Web2_HW05/Models/WalmartSearchResult.cs
@@ -0,0 +1,35 @@
+﻿using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web2_HW05.Models
+{
+    public interface IWalmartSearchApi
+    {
+        [Get("/v1/search?query={searchQuery}&format=json&apiKey={apiKey}")]
+        Task <WalmartSearchResult> SearchAsync(string searchQuery, string apiKey);
+    }
+
+    public class WalmartSearchResult
+    {
+        public string Query { get; set; }
+        public int TotalResults { get; set; }
+        public int Start { get; set; }
+        public int NumItems { get; set; }
+        public IEnumerable<WalmartSearchItem> Items { get; set; }
+    }
+
+    public class WalmartSearchItem
+    {
+        public int ItemId { get; set; }
+        public string Name { get; set; }
+        public decimal Msrp { get; set; }
+        public decimal SalePrice { get; set; }
+        public string Upc { get; set; }
+        public string CategoryPath { get; set; }
+        public string ShortDescription { get; set; }
+        public string ThumbnailImage { get; set; }
+    }
+}
